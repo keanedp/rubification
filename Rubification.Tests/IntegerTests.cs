c6e091051accb7bf4930988239d9b654897c94e8
Daniel Keane <keane.dp@gmail.com>
2011-11-06 06:36:35 -0600

    initial commit

---
+++ Rubification.Tests/IntegerTests.cs
@@ -0,0 +1,70 @@
+using System;
+using NUnit.Framework;
+using Rubification.Core;
+
+namespace Rubification.Tests
+{
+	[TestFixture()]
+	public class IntegerTests
+	{
+		[Test()]
+		public void AsAnIntegerICanIterateTenTimes()
+		{
+			int testInteger = 10;
+			int testCounter = 0;
+			testInteger.Times(i => testCounter++);
+			
+			Assert.AreEqual(testInteger, testCounter);
+		}
+		
+		[Test()]
+		public void AsAnIntegerICanCountDownTo()
+		{
+			int testUpperInteger = 10;
+			int testLowerInterger = 5;
+			int testCounter = 0;
+			testUpperInteger.DownTo(testLowerInterger, i => testCounter = i);
+			
+			Assert.AreEqual(testLowerInterger, testCounter);
+		}
+		
+		[Test()]
+		public void AsAnIntegerICanCountUpTo()
+		{
+			int testUpperInteger = 10;
+			int testLowerInterger = 5;
+			int testCounter = 0;
+			testLowerInterger.UpTo(testUpperInteger, i => testCounter = i);
+			
+			Assert.AreEqual(testUpperInteger, testCounter);
+		}
+		
+		[Test()]
+		public void AsAnIntegerICanTestForEven()  
+		{
+			int evenInteger = 2;
+			Assert.IsTrue(evenInteger.IsEven());
+		}
+		
+		[Test()]
+		public void AsAnIntegerICanTestForOdd()  
+		{
+			int evenInteger = 3;
+			Assert.IsTrue(evenInteger.IsOdd());
+		}
+		
+		[Test()]
+		public void AsAnIntegerICanIncrementByOne()
+		{
+			Assert.AreEqual(6, 5.Next());
+			Assert.AreEqual(6, 5.Succ());
+		}
+		
+		[Test()]
+		public void AsAnIntegerICanDeincrementByOne()
+		{
+			Assert.AreEqual(4, 5.Pred());
+		}
+	}
+}
+
---
+++ Rubification/Core/Integer.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace Rubification.Core
+{
+	public static class Integer
+	{
+		#region iterators
+		
+		public static void Times(this int myNumber, Action<int> block)
+		{
+		   for(int i = 0; i < myNumber; i++)
+		   {
+		       block(i);
+		   }
+		}
+		
+	 	public static void DownTo(this int myNumber, int downToNumber, Action<int> block)
+		{
+			while(myNumber >= downToNumber)
+			{
+			   block(myNumber--);
+			}
+		}
+		
+		public static void UpTo(this int myNumber, int upToNumber, Action<int> block)
+		{
+		    while(myNumber <= upToNumber)
+		    {
+		      block(myNumber++);
+		    }
+		}
+		
+		#endregion
+		
+		public static bool IsEven(this int myNumber)
+		{
+		    return (myNumber % 2) == 0;
+		}
+		
+		public static bool IsOdd(this int myNumber)
+		{
+		    return (myNumber % 2) != 0;
+		}
+		
+		public static int Next(this int myNumber)
+		{
+			return ++myNumber;	
+		}
+		
+		public static int Succ(this int myNumber)
+		{
+			return ++myNumber;	
+		}
+		
+		public static int Pred(this int myNumber)
+		{
+			return --myNumber;	
+		}
+	}
+}
